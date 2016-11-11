d92986a7c35635e536740251747f1953b646cfdd
201244076 LDH <7825dfg@naver.com>
2016-11-11 12:30:07 +0900

    click 메세지 이벤트 구현 checkbox 레이아웃 배경색변경 구현

--- app/src/main/java/com/example/ldh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ldh/myapplication/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.ldh.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
+    TextView t1;
+    CheckBox c1;
+    RadioButton r1;
+    LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll = (LinearLayout) findViewById(R.id.Linear1);
+
+        b1 = (Button)findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "안녕?!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t1 = (TextView)findViewById(R.id.textView1);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "누르지마세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        c1 = (CheckBox)findViewById(R.id.checkBox);
+        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked==true){
+                    ll.setBackgroundColor(Color.BLUE);
+                }
+                else ll.setBackgroundColor(Color.WHITE);
+
+            }
+        });
+
     }
 }
